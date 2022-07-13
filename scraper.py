49841f8db53cac15550a1a9b6ca5140117e64e0a
Darren Clark <dclark@trunarrative.com>
2022-07-13 08:57:53 +0100

    Initial Commit

---
+++ scraper.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://www.marvel.com/articles"
+headers = {
+7    &#39;User-Agent&#39;: &#39;Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36&#39;,
+8}
+
+page = requests.get(URL)
+
+# print(page.text)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+results = soup.find(id="card-body__description")
+
+print(results)
