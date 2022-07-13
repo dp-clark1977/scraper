666fe73460fad2989c895a04294579f996864ab5
Darren Clark <dclark@trunarrative.com>
2022-07-13 09:11:41 +0100

    change source

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.marvel.com/articles"
+URL = "https://thedirect.com/MCU/"
 headers = {
 7    &#39;User-Agent&#39;: &#39;Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36&#39;,
 8}
@@ -12,6 +12,6 @@ page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
-results = soup.find(id="card-body__description")
+results = soup.find(id="news-ticker")
 
 print(results)
